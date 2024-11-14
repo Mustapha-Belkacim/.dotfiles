6bcc9f14bd8d7b960eac811ef7b9cc1fa265af78
Mustapha-Belkacim <26289307+Mustapha-Belkacim@users.noreply.github.com>
2024-11-14 21:35:28 +0100

    add install scripts

---
+++ install.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+
+apt update
+
+# oh my bash
+bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)" --unattended
+# ble.sh
+git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
+make -C ble.sh install PREFIX=~/.local
+echo 'source ~/.local/share/blesh/ble.sh' >>~/.bashrc
+apt install stow
+apt install fd-find
+apt install ripgrep
+apt install fzf
+apt install htop
+apt install bat
+apt install exa
+apt install vifm
+apt install neofetch
+apt install tldr
+apt install ncdu
+apt install jhead
+apt install trash-cli
+apt install ffmpeg
+apt install fdupes
+apt install cmus
+
+#programming
+# lazygit
+add-apt-repository ppa:lazygit-team/release
+apt update
+apt install lazygit
+apt install heroku
+apt install inotify-tools
+apt install nasm
+apt install golang-go
+apt install python3
+pip install ipython
+pip install bpython
+pip install ptpython
+pip install virtualenvwrapper
+pip install pipenv
+# rust
+curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh
+cargo install git-delta
+docker
+docker-compose
+
+# Graphical programs
+# google-chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+apt install gimp
+# Fluxgui
+add-apt-repository ppa:nathan-renniewaldock/flux
+apt update
+apt install fluxgui
+# vs code
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+apt install code
+apt install keepassxc
+apt install stellarium
+# safe eyes
+add-apt-repository ppa:slgobinath/safeeyes && apt update && apt install safeeyes
+apt install texlive-full
+apt install texstudio
+# subilme text
+# scan tailor
+# scan tailor advanced
+# gscan2pdf
+add-apt-repository ppa:jeffreyratcliffe/ppa && apt update && apt install gscan2pdf
+ocrfeeder
+kile
+# timeshift
+add-apt-repository -y ppa:teejee2008/timeshift && apt update && apt install timeshift
+apt install -y filelight
+apt install flameshot
+apt install virtualbox
+# Mega Sync
+curl https://mega.nz/linux/MEGAsync/xUbuntu_20.04/amd64/megasync_4.5.3-2.1_amd64.deb -o /tmp/megasync.deb
+apt install /tmp/megasync.deb
+# Mega Sync Dolphin intergration
+curl https://mega.nz/linux/MEGAsync/xUbuntu_20.04/amd64/dolphin-megasync_4.1.1+4.1_amd64.deb -o /tmp/dolphin-megasync.deb
+apt install /tmp/dolphin-megasync.deb
+# Mega cli
+curl https://mega.nz/linux/MEGAsync/xUbuntu_20.04/amd64/megacmd_1.4.0-5.1_amd64.deb -o /tmp/megacmd.deb
+apt install /tmp/megacmd.deb
+# speedcrunch
+wget https://bitbucket.org/heldercorreia/speedcrunch/downloads/SpeedCrunch-0.12-linux64.deb
+dpkg -i SpeedCrunch-0.12-linux64.deb
+
+# OBS studio
+add-apt-repository ppa:obsproject/obs-studio
+apt install obs-studio
+
+# fix open-fprintd issues in kde ubuntu
+# apt remove fprintd
+# add-apt-repository ppa:uunicorn/open-fprintd
+# apt-get update
+# apt install open-fprintd fprintd-clients python3-validity
+
+apt autoremove
--- install_mac.sh
+++ install_mac.sh
@@ -1,23 +1,47 @@
 #!/usr/bin/env bash
 
-# oh-my-bash
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 # homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install stow
+brew update
+brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+# Install a modern version of Bash.
+brew install bash
+brew install bash-completion2
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells
+  chsh -s "${BREW_PREFIX}/bin/bash"
+fi
+
+# oh-my-bash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 # oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 brew install --cask docker
 brew install --cask iterm2
 brew install fd
-brew install rg
+brew install ripgrep
 brew install fzf
+# To install useful key bindings and fuzzy completion:
+${BREW_PREFIX}/opt/fzf/install
 brew install htop
 brew install bat
 brew install lsd
 brew install eza
 brew install tldr
 brew install trash-cli
+brew install vim --with-override-system-vi
+brew install neovim
 brew install vifm
 brew install neovim
 brew install lazygit
@@ -32,8 +56,11 @@ brew install git-delta
 brew install terminal-notifier
 brew install font-hack-nerd-font
 brew install superfile
+brew install stow
 # ble.sh
 git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
 make -C ble.sh install PREFIX=~/.local
 # autocomplete fo zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+brew cleanup
