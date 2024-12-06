2cdacbcca99a78aa3b1163f026ea469e090ce3d1
Mustapha-Belkacim <26289307+Mustapha-Belkacim@users.noreply.github.com>
2024-12-06 18:49:52 +0100

    auto save files neovim

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -3,3 +3,12 @@ require("config.lazy")
 
 vim.keymap.set("n", "<Tab>", ":bnext<CR>")
 vim.keymap.set("n", "<S-Tab>", ":bprev<CR>")
+
+-- Auto-save on file change
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertLeave" }, {
+  callback = function()
+    if vim.bo.modifiable and vim.bo.modified then
+      vim.cmd("write")
+    end
+  end,
+})
