482b6ca259f953c60f060290e33f7920e3444a47
Mustapha-Belkacim <26289307+Mustapha-Belkacim@users.noreply.github.com>
2026-03-15 22:35:09 +0100

    add github copilot cli to install apps mac

--- install_mac.sh
+++ install_mac.sh
@@ -60,6 +60,7 @@ brew install tmux
 brew install stats
 brew install --cask orbstack
 brew install uv
+brew install copilot-cli
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # ble.sh
 git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
